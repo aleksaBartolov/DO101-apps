ef8e638e173d8e02a6cacf135a06a3de3e314d2d
aleksa.bartolov <aleksa.bartolov@asseco-see.rs>
2020-06-29 21:36:13 +0200

    test

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -7,7 +7,8 @@ app.get('/', function (req, res) {
 
 app.get('/mars', function(req, res){
   res.send('Hello from Mars\n');
-})
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
